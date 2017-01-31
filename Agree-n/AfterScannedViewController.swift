319f0402aab307cb4c3937a26250424d8e16aead
Alessandro Armando <alessandropicchi90@gmail.com>
2017-01-31 14:54:45 +0100

    Commit

---
+++ Agree-n/AfterScannedViewController.swift
@@ -0,0 +1,77 @@
+//
+//  AfterScannedViewController.swift
+//  Agree-n
+//
+//  Created by Alessandro Armando on 31/01/2017.
+//  Copyright © 2017 Matteo Palmieri. All rights reserved.
+//
+
+import UIKit
+
+class AfterScannedViewController: UITableViewController {
+
+    var billNumber = "123456789123456789"
+    var billScanned = ""
+    
+    var productName = [String]()
+    var productLocation = [String]()
+    var productType = [String]()
+    var productImage = [UIImage]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if billScanned == billNumber {
+            let productList = self.loadProducts()
+            for value in productList! {
+                if ((value.AllDetail.productBill?.compare(self.billNumber)) != nil) {
+                    self.productName += [value.AllDetail.productName!]
+                    self.productLocation += [value.AllDetail.productLocation!]
+                    self.productType += [value.AllDetail.productType!]
+                    self.productImage += [value.AllDetail.productImage!]
+                }
+            }
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+    private func loadProducts() -> [BillDetail]? {
+        return NSKeyedUnarchiver.unarchiveObject(withFile: BillDetail.archiveURL.path) as? [BillDetail]
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return productName.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "ProductContentCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! ProductContentCell
+        cell.nameLabel?.text = self.productName[indexPath.row]
+        cell.locationLabel?.text = self.productLocation[indexPath.row]
+        cell.typeLabel?.text = self.productType[indexPath.row]
+        cell.productImage.image = self.productImage[indexPath.row]
+        
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Agree-n/HomeScreenViewController.swift
+++ Agree-n/HomeScreenViewController.swift
@@ -12,13 +12,50 @@ class HomeScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        let checkForAllSampleProducts = self.loadProducts()
+        /* Se si vuole aggiungere un prodotto di esempio cambiare "==" -> "!=" .
+         Così facendo verrà effettuata la sovrascrittura forzata dei prodotti*/
+        if checkForAllSampleProducts == nil {
+            let sample1 = BillDetail(name: "Esempio 1", location: "Locazione esempio 1", type: "Tipo esempio 1", bill: "123456789123456789", image: UIImage(named: "acqua_buxton"))!
+            let sample2 = BillDetail(name: "Esempio 2", location: "Locazione esempio 2", type: "Tipo esempio 2", bill: "123456789123456789", image: UIImage(named: "bacon"))!
+            let sample3 = BillDetail(name: "Esempio 3", location: "Locazione esempio 3", type: "Tipo esempio 3", bill: "123456789123456789", image: UIImage(named: "beer"))!
+            let sample4 = BillDetail(name: "Esempio 4", location: "Locazione esempio 4", type: "Tipo esempio 4", bill: "123456789123456789", image: UIImage(named: "cocacola_lattina"))!
+            let sample5 = BillDetail(name: "Esempio 5", location: "Locazione esempio 5", type: "Tipo esempio 5", bill: "123456789123456789", image: UIImage(named: "formaggio"))!
+            let sample6 = BillDetail(name: "Esempio 6", location: "Locazione esempio 6", type: "Tipo esempio 6", bill: "123456789123456789", image: UIImage(named: "mortadellina"))!
+            self.saveProducts(list: [sample1, sample2, sample3, sample4, sample5, sample6])
+        } /*else {
+            for value in checkForAllSampleProducts! {
+                if value.AllDetail.productBill! == "123456789123456789" {
+                    print(value.AllDetail.productName!)
+                    print(value.AllDetail.productLocation!)
+                    print(value.AllDetail.productType!)
+                    print(value.AllDetail.productImage!)
+                }
+            }
+         }*/
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func saveProducts (list products: [BillDetail]) {
+        let isSuccesufulSave = NSKeyedArchiver.archiveRootObject(products, toFile: BillDetail.archiveURL.path)
+        if !isSuccesufulSave {
+            print("Some problems in saving state...")
+        }
+    }
+    
+    func loadSampleProducts() -> [BillDetail]? {
+        return self.loadProducts()
+    }
+    
+    func loadProducts() -> [BillDetail]? {
+        return NSKeyedUnarchiver.unarchiveObject(withFile: BillDetail.archiveURL.path) as? [BillDetail]
     }
     
 
