3b93f201b788c356f603649d6aebe5c9cbc6d328
Alessandro Armando <alessandropicchi90@gmail.com>
2017-02-02 17:54:43 +0100

    Modificata tabella dei prodotti

--- Agree-n/HomeScreenViewController.swift
+++ Agree-n/HomeScreenViewController.swift
@@ -25,20 +25,25 @@ class HomeScreenViewController: UIViewController {
         let checkForAllSampleProducts = self.loadProducts()
         /* Se si vuole aggiungere un prodotto di esempio cambiare "==" -> "!=" .
          Così facendo verrà effettuata la sovrascrittura forzata dei prodotti*/
-        if checkForAllSampleProducts == nil {
-            let sample1 = BillDetail(name: "Esempio 1", location: "Locazione esempio 1", type: "Tipo esempio 1", bill: "123456789123456789", image: UIImage(named: "acqua_buxton"))!
-            let sample2 = BillDetail(name: "Esempio 2", location: "Locazione esempio 2", type: "Tipo esempio 2", bill: "123456789123456789", image: UIImage(named: "bacon"))!
-            let sample3 = BillDetail(name: "Esempio 3", location: "Locazione esempio 3", type: "Tipo esempio 3", bill: "123456789123456789", image: UIImage(named: "beer"))!
-            let sample4 = BillDetail(name: "Esempio 4", location: "Locazione esempio 4", type: "Tipo esempio 4", bill: "123456789123456789", image: UIImage(named: "cocacola_lattina"))!
-            let sample5 = BillDetail(name: "Esempio 5", location: "Locazione esempio 5", type: "Tipo esempio 5", bill: "123456789123456789", image: UIImage(named: "formaggio"))!
-            let sample6 = BillDetail(name: "Esempio 6", location: "Locazione esempio 6", type: "Tipo esempio 6", bill: "123456789123456789", image: UIImage(named: "mortadellina"))!
-            self.saveProducts(list: [sample1, sample2, sample3, sample4, sample5, sample6])
+        if checkForAllSampleProducts != nil {
+            let sample1 = BillDetail(name: "Uova AMADORI", description: "Da aggiungere", rating: "2.5", bill: "123456789123456789", price: "2.00€", image: UIImage(named: "uova_amadori"))!
+            let sample2 = BillDetail(name: "Arrosto di vitello", description: "Da aggiungere", rating: "2.0", bill: "123456789123456789", price: "15.00€", image: UIImage(named: "arrosto_di_vitello"))!
+            let sample3 = BillDetail(name: "Grano saraceno ECOR", description: "Da aggiungere", rating: "4.5", bill: "123456789123456789", price: "2.80€", image: UIImage(named: "grano_saraceno_ecor"))!
+            let sample4 = BillDetail(name: "Noci sorrento FATINA", description: "Da aggiungere", rating: "4.0", bill: "123456789123456789", price: "2.89€", image: UIImage(named: "noci_fatina"))!
+            let sample5 = BillDetail(name: "Latte intero BERNA", description: "Da aggiungere", rating: "2.5", bill: "123456789123456789", price: "1.60€", image: UIImage(named: "latte_berna_intero"))!
+            let sample6 = BillDetail(name: "Petto di Pollo AMADORI", description: "Da aggiungere", rating: "2.0", bill: "123456789123456789", price: "10.00€", image: UIImage(named: "petto_pollo_amadori"))!
+            let sample7 = BillDetail(name: "Macine MULINO BIANCO", description: "Da aggiungere", rating: "2.0", bill: "123456789123456789", price: "1.70€", image: UIImage(named: "macine_mulino_bianco"))!
+            let sample8 = BillDetail(name: "Nutella", description: "Da aggiungere", rating: "1.5", bill: "123456789123456789", price: "7.50€", image: UIImage(named: "nutella"))!
+            let sample9 = BillDetail(name: "Marmellata Ciliegia Zuegg", description: "Da aggiungere", rating: "3.0", bill: "123456789123456789", price: "2.20€", image: UIImage(named: "mermellata_ciliegia_zuegg"))!
+            let sample10 = BillDetail(name: "Tofu bio CEREAL BIO", description: "Da aggiungere", rating: "5.0", bill: "123456789123456789", price: "10.00€", image: UIImage(named: "tofu_bio_cerealbio"))!
+            let sample11 = BillDetail(name: "Lenticchie SELECT", description: "Da aggiungere", rating: "4.5", bill: "123456789123456789", price: "2.80€", image: UIImage(named: "lenticchie_select"))!
+            
+            self.saveProducts(list: [sample1, sample2, sample3, sample4, sample5, sample6, sample7, sample8, sample9, sample10, sample11])
         } /*else {
             for value in checkForAllSampleProducts! {
                 if value.AllDetail.productBill! == "123456789123456789" {
                     print(value.AllDetail.productName!)
-                    print(value.AllDetail.productLocation!)
-                    print(value.AllDetail.productType!)
+                    print(value.AllDetail.productPrice!)
                     print(value.AllDetail.productImage!)
                 }
             }
