297855a580d469e33635e116fcb00d2f42f3f0c1
Carbone Rosario <carbone.rosario@gmail.com>
2017-02-02 16:05:26 +0100

    Change navigationbar background to .bartintcolor

--- Agree-n/LoginController.swift
+++ Agree-n/LoginController.swift
@@ -99,7 +99,7 @@ class LoginController: UIViewController, UICollectionViewDataSource, UICollectio
         
         let checkUsers = self.loadUsers()
         if checkUsers != nil {
-            let sampleUser = UserDetail(name: "alex", password: "123456789", image: UIImage())
+            let sampleUser = UserDetail(name: "a", password: "a", image: UIImage())
             self.saveUsers(list: [sampleUser!])
         } /*else {
             for user in checkUsers! {
